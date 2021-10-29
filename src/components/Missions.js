2cdb13d34476000dc544c59db1c40c1d0d8aad0e
Lucas Alves <lucasdolohov@gmail.com>
2021-10-29 18:02:50 -0300

    requisito 7

---
+++ src/components/Missions.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Mission extends Component {
+  render() {
+    return (
+      <div data-testid="missions" />
+    );
+  }
+}
+
+export default Mission;
