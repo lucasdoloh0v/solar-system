587f2d0d97eaf9e11fbf44b0ea81d83525f3ed48
Lucas Alves <lucasdolohov@gmail.com>
2021-10-29 16:08:07 -0300

    requisito 4

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <p>salve</p>
+        <Title headline="Planetas" />
       </div>
     );
   }
